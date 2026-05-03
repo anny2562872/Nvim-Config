475a07be68f0340a39a899278c8b89557b9a8b76
anny2562872 <andrewjlebovich@gmail.com>
2026-05-03 19:45:33 -0400

    test commit

--- lua/opts.lua
+++ lua/opts.lua
@@ -17,7 +17,6 @@ vim.keymap.set('n', '<A-k>', ':m .-2<CR>==', { silent = true })
 vim.keymap.set('v', '<A-j>', ":m '>+1<CR>gv=gv", { silent = true })
 vim.keymap.set('v', '<A-k>', ":m '<-2<CR>gv=gv", { silent = true })
 
-
 vim.api.nvim_create_autocmd("FileType", {
     pattern = "c",
     callback = function()
